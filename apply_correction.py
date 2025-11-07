6ce231906e0cf8ecd9c9c984ff390743d834e342
Elaf Musa <elaf.musa@desy.de>
2025-11-08 00:54:49 +0100

    use integerated strength

--- apply_correction.py
+++ apply_correction.py
@@ -6,10 +6,24 @@
 
 import json
 from interface import Interface
+import numpy as np
 ebs = Interface()
-delta_q_s =  json.load(open("./PATH/output/delta_quad_skew.json")) ##
+delta_q_s =  json.load(open("./pySC_tests/LOCO/output/quad_skew_deltas_lengths.json")) ##
+
+# Normal quads
+normal_deltas = delta_q_s["normal_quads"]["delta"]
+normal_lengths = delta_q_s["normal_quads"]["length"]
+
+# Skew quads
+skew_deltas = delta_q_s["skew_quads"]["delta"]
+skew_lengths = delta_q_s["skew_quads"]["length"]
+
 
 name = SC.magnet_settings.index_mapping[elem_ind]+comp ##
+
+
+trim_normal = np.array(list(normal_deltas.values())) * np.array(list(normal_lengths.values()))
+trim_skew   = np.array(list(skew_deltas.values()))   * np.array(list(skew_lengths.values()))
 
 names = []
 trims = []
